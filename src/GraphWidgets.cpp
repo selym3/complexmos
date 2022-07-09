1f5c0ba26c90c0704d6c2c947e785efa52a59ae3
Myles Pasetsky <mp3blue99@gmail.com>
2022-07-09 18:43:44 -0400

    Add test complex map drawing

--- src/GraphWidgets.cpp
+++ src/GraphWidgets.cpp
@@ -2,25 +2,59 @@
 using namespace complexmos;
 
 #include "imgui.h"
+#include "implot.h"
+#include <iostream>
+
+#include <cmath>
+GraphWidget::GraphWidget()
+{
+    for (int i = 0; i < 360; ++i)
+    {
+        inputs.push_back(std::polar(3.0, i * M_PI / 180.0));
+    }
+}
+
+std::complex<double> GraphWidget::f(const std::complex<double>& z) const
+{
+    return std::sin(z) * std::tan(z);
+}
 
 void GraphWidget::operator()(State& state)
 {
+    std::vector<double> ix, iy;
+    std::vector<double> ox, oy;
+    for (const auto& input : inputs)
+    {
+        auto output = f(input);
 
+        // std::cout << "f(" << input << ") = ";
+        ix.push_back(input.real()); 
+        iy.push_back(input.imag());
 
+        // std::cout << output << "\n";
+        ox.push_back(output.real());
+        oy.push_back(output.imag());
+    }
 
+    ImGui::Begin("Graph");
+    if (ImPlot::BeginPlot("My Plot")) 
+    {
+        ImPlot::PlotLine("input", ix.data(), iy.data(), ix.size());
+        ImPlot::PlotLine("output", ox.data(), oy.data(), ox.size());
+        ImPlot::EndPlot();
+    }
+
+    ImGui::End();
 }
 
-
-#include <iostream>
 void GraphSettingsWidget::operator()(State& state)
 {
     ImGui::Begin("Settings");
 
     ImGui::ListBoxHeader("Graph Mode");
-    std::cout << state.modes.size() << std::endl;
     for (std::size_t i = 0; i < state.modes.size(); ++i)
     {
-        if (ImGui::Selectable(state.modes.at(i).c_str(), state.selected_mode == i))
+        if (ImGui::Selectable(state.modes[i].c_str(), state.selected_mode == i))
         {
             state.selected_mode = i;
         }
--- src/GraphWidgets.hpp
+++ src/GraphWidgets.hpp
@@ -3,11 +3,17 @@
 
 #include "State.hpp"
 
+#include <complex>
+
 namespace complexmos
 {
 
     struct GraphWidget
     {
+        std::vector<std::complex<double>> inputs;
+        GraphWidget();
+
+        std::complex<double> f(const std::complex<double>& z) const;
         void operator()(State& state);
     };
 
